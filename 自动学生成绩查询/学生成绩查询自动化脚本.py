6b923f408e7fa5caf0241e5a4ce933252fc10a28
Horizon00001 <zhujunnnnnnn@outlook.com>
2025-07-18 20:20:54 +0800

    Update 学生成绩查询自动化脚本.py

--- 自动学生成绩查询/学生成绩查询自动化脚本.py
+++ 自动学生成绩查询/学生成绩查询自动化脚本.py
@@ -8,7 +8,7 @@ import time
 mydict = {}
 
 # 读取第一个文件"学号.txt"，提取学号和姓名
-with open('Python\自动学生成绩查询\学号.txt', 'r', encoding='utf-8') as ifile:
+with open('.\自动学生成绩查询\学号.txt', 'r', encoding='utf-8') as ifile:
     for line in ifile:
         l = line.split()  # 按空格分割每行
         if '3024244' not in l[0]:  # 筛选特定学号段
@@ -17,14 +17,14 @@ with open('Python\自动学生成绩查询\学号.txt', 'r', encoding='utf-8') a
         #print(mydict[l[0]])
 
 # 读取第二个文件"学号转专业.txt"，补充或更新学号和姓名
-with open('Python\自动学生成绩查询\学号转专业.txt', 'r', encoding='utf-8') as ifile2:
+with open('.\自动学生成绩查询\学号转专业.txt', 'r', encoding='utf-8') as ifile2:
     for line in ifile2:
         l = line.split()  # 按空格分割每行
         mydict[l[0]] = l[1]  # 更新字典中的学号姓名对应关系
         #print(mydict[l[0]])
 
 # 创建或打开"成绩.txt"文件，写入表头
-with open('Python\自动学生成绩查询\成绩.txt', 'a', encoding='utf-8') as f:
+with open('.\自动学生成绩查询\成绩.txt', 'a', encoding='utf-8') as f:
     f.write('学号,姓名,加权,排名' + '\n')  # 写入
 
 # 遍历字典中的每个学号
@@ -60,7 +60,7 @@ for i in mydict.keys():
     element_right = wd.find_elements(By.CLASS_NAME, 'right_cell')
 
     # 将成绩信息写入文件
-    with open('Python\自动学生成绩查询\成绩.txt', 'a', encoding='utf-8') as f:
+    with open('.\自动学生成绩查询\成绩.txt', 'a', encoding='utf-8') as f:
             for i in range(len(element_right)):
                 f.write(element_right[i].text + ', ')  # 写入每个成绩项
             f.write('\n')  # 换行
