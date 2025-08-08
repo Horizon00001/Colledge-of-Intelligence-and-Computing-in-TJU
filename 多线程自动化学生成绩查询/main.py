6e95cf1fceb81551ae32abd9f335246598014ac2
Horizon00001 <zhujunnnnnnn@outlook.com>
2025-08-08 23:21:51 +0800

    Update main.py
    8.8 日更新大幅提高查询速度

--- 多线程自动化学生成绩查询/main.py
+++ 多线程自动化学生成绩查询/main.py
@@ -1,4 +1,4 @@
-# 学生成绩查询自动化脚本
+import sys
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -9,52 +9,59 @@ import column
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from tqdm import tqdm
+from configparser import ConfigParser
+from random import uniform
 
 
 opt = Options()
-# 忽略证书错误
-opt.add_argument('--ignore-certificate-errors')
-# 忽略 Bluetooth: bluetooth_adapter_winrt.cc:1075 Getting Default Adapter failed. 错误
-opt.add_experimental_option('excludeSwitches', ['enable-automation'])
-# 忽略 DevTools listening on ws://127.0.0.1... 提示
-opt.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-opt.add_argument('--headless=new')   
+opt.add_argument('--allow-insecure-localhost') 
+opt.add_argument("--incognito")
+opt.add_argument('-ignore-certificate-errors')
+opt.add_argument('-ignore -ssl-errors')
+opt.add_argument('--headless')   
 opt.add_argument('--disable-images')     
-# opt.add_experimental_option("excludeSwitches", ["disable-logging"])  # 关闭 DevTools 日志
 opt.add_argument('--disable-gpu')
+opt.add_argument('--window-size=1920,1080')
 opt.add_argument('--disable-dev-shm-usage')
 opt.add_argument('--disable-software-rasterizer')
 opt.add_argument('--disable-picture-in-picture')
-opt.add_argument('--window-size=1920,1080')
 
-#opt.add_experimental_option('excludeSwitches', ['enable-automation'])
-#opt.add_argument("--log-level=3")  # 0=INFO, 1=WARN, 2=ERROR, 3=FATAL
 
 
 class Work:
     def __init__(self):  
+        self.config = ConfigParser()
+        self.config.read("config.ini", encoding="utf-8")
+        self.url = self.config['Website']['query_url']
+        self.max_tasks = int(self.config["Settings"]["query_max_tasks"])
+        self.internal_page = float(self.config["Settings"]["query_interval_time_page"])
+        print(f"正在查询: {self.url}")
+        print(f"最大进程数为:{self.max_tasks}")
+        print(f"网页预留间隔为:{self.internal_page}")
         self.visited = {}
         self.wb = Workbook()
         self.ws = self.wb.active
-        self.ws.title = 'cic'
-        self.ws.append(['ID', 'Name', 'Score', 'Rank'])
         self.excel_lock = threading.Lock()
+        self.visited_lock = threading.Lock()  
+        self.right_cnt_lock = threading.Lock()
         self.error_cnt = 0
         self.right_cnt = 0
-        # 加载学生信息列数据
         self.column_ID = column.column_ID
         self.column_Name = column.column_Name
         self.visited = {} 
-        print(len(self.column_ID))
 
-    def work(self, i):
+    def work(self, i, wd):
         try:
-            wd = Chrome(options=opt)
-            wd.get('https://z9lbopmd.yichafen.com/qz/138Owtgzjt')
-            #sleep(1)
+            with self.visited_lock:
+                if i  > len(self.column_ID) - 1:
+                    return
+                if self.column_ID[i] in self.visited:
+                    return
+                self.visited[self.column_ID[i]] = True
+
+            wd.get(self.url)
             # 等待页面加载完成
-            webDriverWait = WebDriverWait(wd, 20)
+            webDriverWait = WebDriverWait(wd, self.internal_page)
             webDriverWait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[placeholder="请输入学号"]')))
             # 定位学号和姓名输入框
             element_id = wd.find_element(By.CSS_SELECTOR, '[placeholder="请输入学号"]')
@@ -63,61 +70,125 @@ class Work:
             # 输入学号和姓名
             element_id.send_keys(self.column_ID[i])
             element_name.send_keys(self.column_Name[i])
-            
             # 点击提交按钮
-            element_link = wd.find_element(By.ID, 'yiDunSubmitBtn')
-            element_link.click()
-            # 等待页面加载完成
-            target = wd.window_handles[-1]
-            wd.switch_to.window(target)
-            webDriverWait = WebDriverWait(wd, 20)
-            webDriverWait.until(EC.presence_of_element_located((By.CLASS_NAME, 'right_cell')))
-            '''
-            for handle in wd.window_handles:
-                wd.switch_to.window(handle)
-                if '加权' in wd.title:
-                    break
-            '''
-            # 定位包含成绩信息的元素
-            element_right = wd.find_elements(By.CLASS_NAME, 'right_cell')
-            data = [element.text for element in element_right]
-            with self.excel_lock:
-                self.ws.append(data)
-            self.right_cnt += 1
-            print(f'正在进行第{self.right_cnt}个', data[0])
-            self.visited[self.column_ID[i]] = True
+            def click1():
+                element_link = wd.find_element(By.CSS_SELECTOR, '.kd-button.kd-button-primary.kd-button-xl')
+                element_link.click()
+
+                target = wd.window_handles[-1]
+                wd.switch_to.window(target)
+                
+                webDriverWait = WebDriverWait(wd, self.internal_page)
+                webDriverWait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class='src-pc-components-requery-index-module__requery-tips']")))
+                if "查询结果为空" in wd.page_source:
+                    wd.quit()
+                    print(f"{self.column_Name[i]} 学生不存在，结束进程")
+                    sys.exit(0)
+                
+                webDriverWait = WebDriverWait(wd, self.internal_page)
+                webDriverWait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class='src-pc-components-result-field-value-index-module__value']")))
+                print("正在获取...")
+                list = wd.find_elements(By.CSS_SELECTOR, "[class='src-pc-components-result-field-value-index-module__value']")
+                data = [li.text for li in list]
+                with self.excel_lock:
+                    self.ws.append(data)
+                with self.right_cnt_lock:
+                    self.right_cnt += 1
+                print(f"一共 {len(self.column_ID)} 个，第 {self.right_cnt} 个已获取", data)
+                
+                
+
+            def click2():#'Work' object has no attribute 'right
+                element_link = wd.find_element(By.ID, 'yiDunSubmitBtn')
+                element_link.click()
+                # 等待页面加载完成
+                target = wd.window_handles[-1]
+                wd.switch_to.window(target)
+                webDriverWait = WebDriverWait(wd, self.internal_page)
+                webDriverWait.until(EC.presence_of_element_located((By.CLASS_NAME, 'right_cell')))
+                print("正在获取...")
+                element_right = wd.find_elements(By.CLASS_NAME, 'right_cell')
+                data = [element.text for element in element_right]
+                with self.excel_lock:
+                    self.ws.append(data)
+                with self.right_cnt_lock:
+                    self.right_cnt += 1
+                print(f"一共 {len(self.column_ID)} 个，第 {self.right_cnt} 个已获取", data)
+
+            if "yichafen" in self.url:
+                click2()
+            elif "web.wps.cn" in self.url:
+                click1()
+            wd.delete_all_cookies()
+            
         except Exception as e:
             if wd:
-                wd.quit()
-            print("错误：", e)
-            print('正在重新开始该进程', self.column_ID[i], self.column_Name[i])
-            self.work(i)
-
+                print("重新加载网页", e)
+                wd.delete_all_cookies()
+                self.work(i, wd)
+            else:
+                print("重新打开浏览器")
+                wd = Chrome(options=opt)
+                sleep(uniform(1,2))
+                print(e)
+                print('正在重新开始查询：', self.column_ID[i], self.column_Name[i])
+                self.work(i, wd)
         finally:
-            if wd:
-                wd.quit()
+            pass
+            
 
+    
+    def run1(self):
+        wd = Chrome(options=opt)
+        for i in (range(1, len(self.column_ID))):
+            self.work(i, wd)
+        wd.quit()
+    '''
+    def run2(self):
+        wd = Chrome(options=opt)
+        for i in (range(len(self.column_ID))):
+            self.work(len(self.column_ID) - i - 1, wd)
+        wd.quit()
+    def run3(self):
+        wd = Chrome(options=opt)
+        for i in (range(len(self.column_ID))):
+            self.work(len(self.column_ID) // 3 + i, wd)
+        wd.quit()
+    def run4(self):
+        wd = Chrome(options=opt)
+        for i in (range(len(self.column_ID))):
+            self.work(len(self.column_ID) // 3  + i, wd)
+        wd.quit()
+    '''
 
-    def run(self):
+    def thread(self):
         threads = []
-        for i in tqdm(range(1, len(self.column_ID))):
-            if self.column_ID[i] in self.visited:
-                continue
-            t1 = threading.Thread(target=self.work, args=(i,))
-            t1.start()
-            threads.append(t1)
-            sleep(0.5) 
+        total_tasks = len(self.column_ID)
+        pbar = tqdm(total=total_tasks, desc="总进度", unit="次数")
+        for _ in range(self.max_tasks):
+            t = threading.Thread(target=self.run1)
+            t.start()
+            sleep(0.1)
+            threads.append(t)
+        
+        
+        while any(t.is_alive() for t in threads):
+            with self.right_cnt_lock:
+                pbar.n = self.right_cnt 
+                pbar.refresh()
+            sleep(0.1) 
+        pbar.close()
         for t in threads:
             t.join()
-        
+
         self.wb.save('result.xlsx')
-        print(f'成功{self.right_cnt}个，失败{len(self.column_ID) - self.right_cnt}个')
-        # 打印所有失败的学号和姓名
+        print(f'成功{self.right_cnt}个，失败{len(self.column_ID) - 1 - self.right_cnt}个')
         for i in range(1, len(self.column_ID)):
-            if self.column_ID[i] in self.visited:
+            if self.visited[self.column_ID[i]]:
                 continue
-            print(f'错误 (学号: {self.column_ID[i]}, 姓名: {self.column_Name[i]})')
+            print(f'失败 (学号: {self.column_ID[i]}, 姓名: {self.column_Name[i]})')
+
 
 if __name__ == '__main__':
     worker = Work()
-    worker.run()
+    worker.thread()
